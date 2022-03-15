5451515a61c3473f7bcbe937647a72669dad2300
MatheusDev22 <matheusbsb95@gmail.com>
2022-03-15 19:50:13 -0300

    new String( ), the compiler does not handle literal

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -9,8 +9,8 @@ public class Program {
 		Client c1 = new Client("Maria", "maria@gmail.com");
 		Client c2 = new Client("Maria", "maria@gmail.com");
 		
-		String s1 = "Test";
-		String s2 = "Test";
+		String s1 = new String ("Test");
+		String s2 = new String ("Test");
 		System.out.println(c1.hashCode());
 		System.out.println(c2.hashCode());
 		System.out.println(c1.equals(c2));
