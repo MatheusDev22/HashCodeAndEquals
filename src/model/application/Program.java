6b9cc76ea734b86dbd5b059a0f043069cb717cec
MatheusDev22 <matheusbsb95@gmail.com>
2022-03-15 19:40:33 -0300

    using methods hashCode() and equals(), comparing tow attributes

--- src/model/application/Program.java
+++ src/model/application/Program.java
@@ -1,15 +1,18 @@
 package model.application;
 
+import model.entity.Client;
+
 public class Program {
 	
 	public static void main(String[] args) {
 		
-		String a = "Maria";
-		String b = "Alex";
-	 
+		Client c1 = new Client("Maria", "maria@gmail.com");
+		Client c2 = new Client("Maria", "alex@gmail.com");
 		
-		System.out.println(a.hashCode() );
-		System.out.println(b.hashCode() );
+		System.out.println(c1.hashCode());
+		System.out.println(c2.hashCode());
+		System.out.println(c1.equals(c2));
+		
 	}
 
 }
